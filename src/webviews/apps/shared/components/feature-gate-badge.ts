57e05d7df8b3e7c17628f0cec3e8d8d6f9ca1845
Hideki Tanji <tanji@daitogiken.com>
2023-07-18 11:24:36 +0900

    好みの修正。

--- src/webviews/apps/shared/components/feature-gate-badge.ts
+++ src/webviews/apps/shared/components/feature-gate-badge.ts
@@ -2,9 +2,7 @@ import { css, html, LitElement, nothing } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import type { Subscription } from '../../../../subscription';
 import {
-	getSubscriptionStatePlanName,
 	getSubscriptionTimeRemaining,
-	isSubscriptionStatePaidOrTrial,
 	isSubscriptionStateTrial,
 	SubscriptionState,
 } from '../../../../subscription';
@@ -84,55 +82,6 @@ export class FeatureGateBadge extends LitElement {
 	subscription?: Subscription;
 
 	override render() {
-		const paidOrTrial = isSubscriptionStatePaidOrTrial(this.subscription?.state);
-		const trial = isSubscriptionStateTrial(this.subscription?.state);
-
-		return html`
-			<span class="badge-container">
-				<span class="badge ${paidOrTrial ? 'active' : 'inactive'}"
-					>${trial ? html`<span class="badge-trial">Trial</span>` : ''}✨</span
-				>
-				<pop-over .placement=${this.placement} class="badge-popover">
-					<span slot="heading"
-						>${getSubscriptionStatePlanName(
-							this.subscription?.state,
-							this.subscription?.plan.effective.id,
-						)}${this.trialHtml}</span
-					>
-					${this.footnoteHtml}
-				</pop-over>
-			</span>
-		`;
-	}
-
-	private get trialHtml() {
-		if (!isSubscriptionStateTrial(this.subscription?.state)) return nothing;
-
-		const days = getSubscriptionTimeRemaining(this.subscription!, 'days') ?? 0;
-		return html`<span class="badge-trial-left">${days < 1 ? '<1 day' : pluralize('day', days)} left</span>`;
-	}
-
-	private get footnoteHtml() {
-		switch (this.subscription?.state) {
-			case SubscriptionState.VerificationRequired:
-			case SubscriptionState.Free:
-			case SubscriptionState.FreePreviewTrialExpired:
-				return html`<span class="badge-footnote"
-					>✨ Requires a trial or paid plan for use on privately hosted repos.</span
-				>`;
-			case SubscriptionState.FreePlusTrialExpired:
-			case SubscriptionState.FreeInPreviewTrial:
-			case SubscriptionState.FreePlusInTrial:
-				return html`<span class="badge-footnote"
-					>✨ Requires a paid plan for use on privately hosted repos.</span
-				>`;
-			case SubscriptionState.Paid:
-				/* prettier-ignore */
-				return html`<span class="badge-footnote"
-					>You have access to ✨ features on privately hosted repos and ☁️ features based on your plan.</span
-				>`;
-			default:
-				return nothing;
-		}
+		return undefined;
 	}
 }
